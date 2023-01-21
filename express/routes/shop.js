323405275c14026d57d68842535f89891d2f239e
AgoroAkorede <agoroakorede@gmail.com>
2023-01-21 09:49:18 +0100

    added main-layout

--- express/routes/shop.js
+++ express/routes/shop.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
-const adminData = require("./admin")
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
