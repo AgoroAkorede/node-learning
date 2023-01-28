c1dc25dc56413bdb250e1b2620a73f698cbbe6dd
AgoroAkorede <agoroakorede@gmail.com>
2023-01-28 19:43:01 +0100

    add: sequelize

--- express/util/database.js
+++ express/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Oluwarotimi02'
+const sequelize = new Sequelize("node-complete", "root", "Oluwarotimi02", {
+  dialect: "mysql",
+  host: "localhost",
 });
 
-module.exports = pool.promise();
+module.export = sequelize;
