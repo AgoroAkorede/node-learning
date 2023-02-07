787b82f2b44703fd9dffc06dffd9397c7b1ecde4
AgoroAkorede <agoroakorede@gmail.com>
2023-02-07 18:50:42 +0100

    feat: Error handling

--- express/app.js
+++ express/app.js
@@ -40,11 +40,11 @@ app.use(
 );
 app.use(csrfProtection);
 app.use(flash());
-
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locsls.csrfToken = req.csrfToken();
 });
+
 
 app.use((req, res, next) => {
   if (!req.session.user) {
