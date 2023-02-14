a77293b6f655a551bb9548e184ce6e62f7dd0a51
AgoroAkorede <agoroakorede@gmail.com>
2023-02-14 10:10:35 +0100

    feat: added the frontend

---
+++ backend/controllers/feed.js
@@ -0,0 +1,32 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/duck.jpg',
+        creator: {
+          name: 'Korede'
+        },
+        createdAt: new Date()
+      }
+    ]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: 'Korede' },
+      createdAt: new Date()
+    }
+  });
+};
