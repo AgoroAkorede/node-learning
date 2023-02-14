077208146299ebb3a528161f41874129b2b5c6da
AgoroAkorede <agoroakorede@gmail.com>
2023-02-14 22:32:01 +0100

    feat: added pagination

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
 
 const app = express();
 
@@ -9,12 +10,21 @@ const app = express();
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
-
-app.listen(8080);
+app.use("/feed", feedRoutes);
+mongoose
+  .connect(
+    "mongodb+srv://KOREDE:*9$3ztAWUG8SQaf@cluster0.gpoozsa.mongodb.net/messages"
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,32 +1,69 @@
+const { validationResult } = require("express-validator");
+const Post = require("../modals/post");
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/duck.jpg',
-        creator: {
-          name: 'Korede'
-        },
-        createdAt: new Date()
-      }
-    ]
-  });
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
+  Post,
+    find()
+      .countDocuments()
+      .then((count) => {
+        totalItems = count;
+        return Post.find()
+          .skip((currentPage - 1) * perPage)
+          .limit(perPage);
+      })
+      .then((posts) => {
+        res.status(200).json({
+          message: "Fetched products succesfully",
+          posts: posts,
+          totalItems: totalItems,
+        });
+      })
+      .catch((err) => {
+        if (err.statusCode) {
+          err.statusCode = 500;
+        }
+      });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "validaition failed, entered data is incorrect",
+      errors: errors.array(),
+    });
+  }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
+  const post = new Post({
+    _id: new Date().toISOString(),
+    title: title,
+    content: content,
+    creator: { name: "Korede" },
+  });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: {
+          _id: new Date().toISOString(),
+          creator: { name: "Korede" },
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   res.status(201).json({
-    message: 'Post created successfully!',
+    message: "Post created successfully!",
     post: {
       _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: 'Korede' },
-      createdAt: new Date()
-    }
+      creator: { name: "Korede" },
+    },
   });
 };
